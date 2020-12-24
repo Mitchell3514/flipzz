b6dc3400504436e379879373964e66bdd817facd
Mitchell <contact@mitchells.work>
2020-12-24 21:05:56 +0100

    use boilerplate code

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@ const cleanCSS = require("gulp-clean-css");
 const minify = require("gulp-minify");
 const webp = require('gulp-webp');
 const livereload = require("gulp-livereload");
+const clean = require("gulp-clean");
 
 const path_in = "src";
 const path_out = "dist";
 
-const out = (file) => { return file.base.replace(path_in, path_out); };
+const out = (file) => { return file.base.replace(path_in, path_out);  };
+
+function clear() { 
+    return src("./dist", { allowEmpty: true })
+        .pipe(clean({ force: true }));
+}
 
 function views() {
     return src("src/public/views/*.ejs")
@@ -23,18 +29,16 @@ function css() {
 }
 
 function js() {
-    return src("src/public/js/*.js")
+    return src(["src/public/js/*.js", "src/bin/www"])
         .pipe(minify({
-            ext: {
-                min: ".js",
-            }
+            ext: ".js",
         }))
         .pipe(dest(out))
         .pipe(livereload());
 }
 
 function assets() {
-    return src("src/public/assets/*.{png,gif,jpg}")
+    return src("src/public/assets/*.*")
         .pipe(webp())
         .pipe(dest(out))
         .pipe(livereload());
@@ -55,5 +59,5 @@ function livewatch() {
     watch("src/**/*.js", { ignored: "src/public" }, parallel(server));
 }
 
-exports.dev = series(parallel(views, css, js, assets, server), livewatch);
-exports.build = parallel(views, css, js, assets, server);
+exports.dev = series(clear, parallel(views, css, js, assets, server), livewatch);
+exports.build = series(clear, parallel( views, css, js, assets, server));
