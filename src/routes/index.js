5b28596412ed9aba9d18f611b864e906308f8079
katycatxnadine <you@example.com>
2020-12-27 23:57:37 +0100

    hoi

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,11 @@ const stats = require("../public/assets/stats.json");
 /* GET home page. */
 router.get("/", (req, res) => {
 	const { games, flipped } = stats; 
+	// parameters sent to server (to render)
 	res.render("index", { title: 'Express', connections: connections.length, games, flipped }); // eslint-disable-line
 });
 
-/* GET game page */
+/* GET game page after pressing PLAY */
 router.get("/game", (req, res) => {
 	res.render("game");
 });
