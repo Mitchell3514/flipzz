0447808312eaa78f4bef000c5bfff78606a836e4
Mitchell <contact@mitchells.work>
2021-01-01 22:57:30 +0100

    cfg

--- src/public/js/util/config.js
+++ src/public/js/util/config.js
@@ -1,4 +1,4 @@
 // Code shared between client and server: game setup.
 (function(exports) {
     exports.boardsize = 8;
-})(exports === "undefined" ? (this.CFG = {})  : exports);
+})(typeof exports === "undefined" ? (this.CFG = {})  : exports);
