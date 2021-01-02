d0b6c2c5aa7e05888fb6cf79c12613a4d2f5f4e4
Mitchell <contact@mitchells.work>
2021-01-02 20:59:23 +0100

    fix pos

--- src/public/js/util/position.js
+++ src/public/js/util/position.js
@@ -1,11 +1,13 @@
 (function (exports) {
-    exports.Position = function Position(id) {
+    function Position(id) {
         this.id = id;
     
         this.color = null;
         this.setColor = (color) => this.color = color;
         this.isTaken = () => this.color !== null;
     }
+
+    exports.Position = Position;
     // if client-side: exports = this.Position where this refers to Window
     // if server-side: exports is just module.exports
 })(typeof exports === "undefined" ? (this.Classes = {}) : exports)
