e682b1f8460d76935ceb889d5efd396dcc4cfdda
Mitchell <contact@mitchells.work>
2021-01-03 21:45:23 +0100

    add fun room name to games

--- src/archetypes/gameHandler.js
+++ src/archetypes/gameHandler.js
@@ -1,6 +1,32 @@
 // @ts-check
+const CFG = require("../public/js/util/config");
 const Board = require("../public/js/util/board").Board;     // object (function)
-const gameStats = require("../public/assets/stats.json");
+const { writeFile } = require("fs"); // @ts-expect-error ROOT not defined globally 
+const dataURI = require("path").join(ROOT, "../stats.json"); // eslint-disable-line
+
+const https = require("https");
+const names = [];
+function getName() {
+    if (names.length) return Promise.resolve(names.splice(0, 1)[0]);
+    return fetchNames();
+}
+
+function fetchNames() {
+    return new Promise((resolve, reject) => {
+        https.get("https://api.fungenerators.com/name/generate.json?category=alien").on("response", (response) => {
+            let str = "";
+            response.on("data", data => str += data);
+            response.once("end", () => {
+                try {
+                    const nameArray = JSON.parse(str).contents.names;
+                    const name = nameArray.splice(0, 1)[0];
+                    names.push(...nameArray);
+                    resolve(name);
+                } catch (e) { console.error(`Unable to get random names\n${e}`); reject(e); }
+            });
+        });
+    });
+}
 
 /**
  * @typedef {import("./connectionHandler").ExtendedConnection} EC
@@ -19,27 +45,27 @@ const gameStats = require("../public/assets/stats.json");
 
 function Game(id) {
     this.id = id;
+    getName().then(name => this.name = name);
     this.status = -1;
 
-    this.board = new Board(8, 8);
+    this.board = new Board(CFG.boardsize, CFG.boardsize);
     this.board.init();
 
     this.dark = null;       // dark and light are both assigned ws connections
     this.light = null;
     this.turn = 0;      // 0 is dark, 1 is light
 
-
-    // TODO update stats: amount of flipped pieces sent by flipzz - gameOver()
-    gameStats.flipped ++;
-    
+    // stats
+    this.flipped = 0;
+   
 
     this.isFull = () => this.light && this.dark;
     
     // player added to game if not full yet, else return false
     // first added player (connection) is dark
     this.addPlayer = (/** @type {EC & import("ws")} */ connection) => {
-        if (!this.dark) return (this.dark = connection, this._send(0, { id: this.id }), true);
-        if (!this.light) return (this.light = connection, this._send(1, { id: this.id }), this._start(), true);     // after player 2 (light) is added, game starts
+        if (!this.dark) return (this.dark = connection, this._send(0, { gameID: this.id, name: this.name }), true);
+        if (!this.light) return (this.light = connection, this._send(1, { gameID: this.id, name: this.name }), this._start(), true);     // after player 2 (light) is added, game starts
         return false;
     };
 
@@ -61,15 +87,15 @@ function Game(id) {
         if (result.some(pos => pos.id === data.position)) {       // if the position sent is placeable
             payload.valid = true;
 
-            this.board.place(data.position, color); // update board: (pos.id, color)
+            const flipped = this.board.place(data.position, color); // update board: (pos.id, color)
+            this.flipped += (flipped.length - 1);
             
             // player can place, see if next player can place
             const canPlay = this.board.canPlace(+!color);       // array of all Positions where can be placed by other player
 
             if (!canPlay) { // other player can't place - send who won
                 this.status++; // status 1 --> status 2
-                // update stats: games completed
-                gameStats.games++;      
+
                 // ---------- IF GAME COMPLETED --------------------------------------
                 return this._send(2, { winner: this.board.winner(), ...payload });      // 2 = send to both --> winner (0/1) + payload (pos id)
             }
@@ -85,9 +111,11 @@ function Game(id) {
         return true;
     };
 
-    this.stop = (/** @type {number} */ id) => {
+    this.leave = (/** @type {number} */ id) => {
+        if (!this.light && this.dark?.id === id) return this.dark = null;
         this.status = 3;       // aborted --> status 3
-        this._send(+!id, {message: "The other player has left the game."});       // inform other player who is left
+        this._send(+!(id === this.light.id), {message: "The other player has left the game."});       // inform other player who is left
+        this._updateStats({ flipped: this.flipped });
     };
 
     // payload is an object containing data
@@ -95,7 +123,7 @@ function Game(id) {
     // 1 --> send to light
     // 2 --> send to both   (id-1 guarantees this)
     this._send = (id, payload = {}) => {
-        const msg = JSON.stringify({ id: this.id, status: this.status, ...payload });    // concatenated output string: {"status":0,"player":0,"turn":0} 
+        const msg = JSON.stringify({ status: this.status, ...payload });    // concatenated output string: {"status":0,"player":0,"turn":0} 
         if (id-1) this.dark.send(msg);      
         if (id) this.light.send(msg);
     };
@@ -105,6 +133,13 @@ function Game(id) {
         this._send(0, { player: 0, turn: this.turn });  // sent to dark: payload contains player type and turn
         this._send(1, { player: 1, turn: this.turn });  // sent to light
         this.status++;                                  // status 0 sent, but now becomes status 1 (game continuing)
+        this._updateStats({ games: 1 });
+    };
+
+    this._updateStats = (obj) => {
+        const file = require(dataURI);
+        Object.keys(obj).forEach(key => file[key] !== undefined ? file[key] += obj[key] : file[key] = obj[key]);
+        writeFile(dataURI, JSON.stringify(file), (e) => { if (e) console.log(e || `Updated stats: ${file}`); });
     };
 }
 
