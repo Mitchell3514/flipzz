5379cd3fd1853b559effa90caa8c3d52f5c3beda
Mitchell <contact@mitchells.work>
2021-01-06 19:03:14 +0100

    run server with concurrently again

--- dev.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-echo Starting gulp compiling. 
-CMD /c gulp build 
-echo Finished initial build, launching gulp watch in the background. 
-start /min CMD /c gulp dev 
-echo Now starting server 
-sleep 1 
-nodemon 
-exit 0
